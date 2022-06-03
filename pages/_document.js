aed32b7b8ffc2e781e66ab2ee8e5e3df8f34581a
周公来 <zhougonglai@nn.com>
2022-06-04 00:45:21 +0800

    init proj

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import { ColorModeScript, extendTheme } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          {/* 👇 Here's the script */}
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
